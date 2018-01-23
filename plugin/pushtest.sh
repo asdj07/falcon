0ecb100e6ed6a028bf71feb57169eed323fb1c18
Johnny <290410495@qq.com>
2018-01-23 16:40:30 +0800

    Delete pushtest.sh
    delete

--- plugin/pushtest.sh
+++
@@ -1,4 +0,0 @@
-ts=`date +%s`;
-
-echo "[{\"metric\": \"test-metric\", \"endpoint\": \"test-endpoint\", \"timestamp\": $ts,\"step\": 60,\"value\": 1,\"counterType\": \"GAUGE\",\"tags\": \"idc=lg,project=xx\"}]" 
-
